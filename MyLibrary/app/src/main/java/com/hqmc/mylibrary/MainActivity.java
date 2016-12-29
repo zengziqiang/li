7df7770fe8194a80b09b724de21717c9c6068d33
曾自强 <272804553@qq.com>
2016-12-29 10:29:51 +0800

    删除了包
    删除了包

--- MyLibrary/app/src/main/java/com/hqmc/mylibrary/MainActivity.java
+++ MyLibrary/app/src/main/java/com/hqmc/mylibrary/MainActivity.java
@@ -1,13 +1,38 @@
 package com.hqmc.mylibrary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.hqmc.mylibrarytest.MainActivity_Library;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tv_jump;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv_jump = (TextView) findViewById(R.id.tv_jump);
+        tv_jump.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_jump:
+                startActivity(new Intent(MainActivity.this, MainActivity_Library.class));
+                break;
+            default:
+
+                break;
+        }
     }
 }
